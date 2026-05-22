a205df4351250872904cf2c5faa2e258ae052dd5
randysaul <randy.saul@yahoo.com.mx>
2026-05-21 20:12:49 -0600

    Cambio nuevo en archivos

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY package*.json .
 COPY app.js .
 RUN npm install
 EXPOSE 5006
-ENV color_fuente=blue
+ENV color_fuente=lightblue
 ENV fuente=Arial
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
         <h1>El color de la fuente es: ${color}</h1>
         <hr>
         <p>Se agrega otro texto</p>
+        <p>Otro texto adicional</p>
       </body>
     </html>
   `);
